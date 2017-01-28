58971e7bb9ca8009f7f50fb23eb8dca24f43d7c5
lorenzomarsicano <lorenzomarsicano@gmail.com>
2017-01-28 08:50:53 +0100

    Fixed import error

--- main.c
+++ main.c
@@ -2,10 +2,18 @@
 #include <SDL2/SDL.h>
 #include "chip8.h"
 
-//chip8 myChip;
+const int SCREEN_HEIGHT = 64;
+const int SCREEN_WIDTH = 32;
 
 int main(int arg, char **args){
+    SDL_Window *window = NULL;
+    SDL_Surface *screenSurface = NULL;
+    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+        printf("Error initializing SDL: %s\n", SDL_GetError());
+    else{
 
+    
+    }
     init();
 
     return 0;
